24bfae8e7f95c9ec537ebddb44cc001a495f0836
Ángel Cruz <langelcruzlara@gmail.com>
2021-07-06 12:06:35 -0500

    feat: progress bar functionality and final video player styles

--- js/index.js
+++ js/index.js
@@ -3,30 +3,40 @@ const $play = document.getElementById('play');
 const $pause = document.getElementById('pause');
 const $backward = document.getElementById('backward');
 const $forward = document.getElementById('forward');
+const $progress = document.getElementById('progress');
 
 $backward.addEventListener('click', handleBackward);
 $play.addEventListener('click', handlePlay);
 $pause.addEventListener('click', handlePause);
 $forward.addEventListener('click', handleForward);
 
+$video.addEventListener('loadedmetadata', handleLoaded);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+$progress.addEventListener('input', handleInput);
+
 function handlePlay() {
   $video.play();
   $play.hidden = true;
   $pause.hidden = false;
 }
-
 function handlePause() {
   $video.pause();
   $play.hidden = false;
   $pause.hidden = true;
 }
-
 function handleBackward() {
   $video.currentTime -= 10;
-  console.log('hello', $video.currentTime);
 }
-
 function handleForward() {
   $video.currentTime += 10;
-  console.log(this, $video.currentTime);
+}
+
+function handleLoaded() {
+  $progress.max = $video.duration;
+}
+function handleTimeUpdate() {
+  $progress.value = $video.currentTime;
+}
+function handleInput() {
+  $video.currentTime = $progress.value;
 }
